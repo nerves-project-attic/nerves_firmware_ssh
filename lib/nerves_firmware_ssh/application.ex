bba9982ca76bb1c32840524b286f214a960cffe6
Frank Hunleth <fhunleth@troodon-software.com>
2017-07-03 16:03:04 -0400

    Remove authorized keys; cleanup

--- lib/nerves_firmware_ssh/application.ex
+++ lib/nerves_firmware_ssh/application.ex
@@ -3,12 +3,6 @@ defmodule Nerves.Firmware.SSH.Application do
 
   use Application
 
-  # FIXME - remove my public keys!!!
-@auth_keys """
-  ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDBCdMwNo0xOE86il0DB2Tq4RCv07XvnV7W1uQBlOOE0ZZVjxmTIOiu8XcSLy0mHj11qX5pQH3Th6Jmyqdjo9WCjf3H/VaAxKtxp0bkL5uAf4tn0W3oU5KmuBoxmkYhcBbs6VG4lgBCsGH9ZnMJ+4JuWtC5s6krzwUgd3pL8JHuMw== fhunleth
-  ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCaf37TM8GfNKcoDjoewa6021zln4GvmOiXqW6SRpF61uNWZXurPte1u8frrJX1P/hGxCL7YN3cV6eZqRiFJKxo4VBRQE4OnWEY6e0+2tN1cwd6FGdBmCndf6GBEm/s6sf0psjcB6tMordrCIzWiZ8fNTCohJihfdgroyhl52c2K1Nkp0FF16+Y0nmv5fobe0r03wTe/oanejF0M3QcCZ5QBEvHK7+CUMhgRhNh5IkcP8lIrwLlHr8p2nFGWhkR6EvYDdYCNCcNruV3dmhQ2/c2zrbuFTqZfdIDlD+jP02YdyPIszkaeRbgPQckxyGOso3Mq0F5ay2OiE0ntYgUolO31FMORfVPgWHf2xKpGp40f3G8FRRvc7xrHGQvr/0KWue6YFrtDPhPhzM9+8EZ7ueMj6eG3ImWXn+GHdsp8v8rc/cCtZ+T8k8Xv2MA+T0CZz4IuPSMB3uVaCmHMSOGdscTsnuOZZLvQ+V17rk+XuvDXVyYMk+J3WRtw/Kr5eI6sFW1G+TKdrE2MbRNuQZBJq0mMzVQfAVYburC1fYBRppcv5SopD/2zeiIXtG+ZPZfYAhbpSU6rAST1oicMnpUKqxCm7u6Zt/790qYLOje+OyxKf6C3fWbQuUU/bu+gzVdyKyYII1EQhkeKrTV2/Q6hIovdMkGLLi+PWWh+YWAna5o8w== fhunleth
-  """
-
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
@@ -22,9 +16,9 @@ defmodule Nerves.Firmware.SSH.Application do
 
   def init() do
     port = Application.get_env(:nerves_firmware_ssh, :port, 8989)
-    authorized_keys = Application.get_env(:nerves_firmware_ssh, :authorized_keys, @auth_keys)
-    decoded_authorized_keys =
-      :public_key.ssh_decode(authorized_keys, :auth_keys)
+    authorized_keys = Application.get_env(:nerves_firmware_ssh, :authorized_keys, "")
+    decoded_authorized_keys = :public_key.ssh_decode(authorized_keys, :auth_keys)
+
     cb_opts = [authorized_keys: decoded_authorized_keys]
 
     {:ok, _ref} = :ssh.daemon(port,
--- lib/nerves_firmware_ssh/keys.ex
+++ lib/nerves_firmware_ssh/keys.ex
@@ -1,6 +1,6 @@
 defmodule Nerves.Firmware.SSH.Keys do
 
-    def host_key(algorithm, options) do
+  def host_key(algorithm, options) do
     # Delegate to system implementation for handling the host keys
     :ssh_file.host_key(algorithm, options)
   end
@@ -10,6 +10,7 @@ defmodule Nerves.Firmware.SSH.Keys do
     cb_opts = Keyword.get(options, :key_cb_private)
     keys = Keyword.get(cb_opts, :authorized_keys)
 
+    # If any of them match, then we're good.
     Enum.any?(keys, fn({k, _info}) -> k == key end)
   end
 end
