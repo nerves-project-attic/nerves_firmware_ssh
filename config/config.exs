b2684745a94190dac98c7f142b54a4162b8a5821
Justin Schneck <jschneck@mac.com>
2017-07-05 22:25:54 -0400

    add config example

---
+++ config/config.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# config :nerves_firmware_ssh,
+#   authorized_keys: [
+#     "ssh-rsa 12345== abcd",
+#     "ssh-rsa 67890== fghi",
+#   ],
+#   system_dir: "/etc/ssh"
+
+# The keys located in /etc/ssh should be named
+#  ssh_host_rsa_key
+#  ssh_host_rsa_key.pub
