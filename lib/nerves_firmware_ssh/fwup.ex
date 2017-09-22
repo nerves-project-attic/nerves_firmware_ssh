8531bf535b328ba17f3ac68f291fe606a72796c5
Frank Hunleth <fhunleth@troodon-software.com>
2017-09-22 11:07:04 -0400

    Random cleanup that breaks things again

--- lib/nerves_firmware_ssh/fwup.ex
+++ lib/nerves_firmware_ssh/fwup.ex
@@ -9,10 +9,7 @@ defmodule Nerves.Firmware.SSH.Fwup do
   end
 
   def send_chunk(pid, chunk) do
-    Logger.error("GenServer.call")
-    rc = GenServer.call(pid, {:send, chunk})
-    Logger.error("GenServer.call done: #{inspect rc}")
-    rc
+    GenServer.call(pid, {:send, chunk})
   end
 
   def init([cm]) do
@@ -29,58 +26,73 @@ defmodule Nerves.Firmware.SSH.Fwup do
        :binary,
        :exit_status
       ])
-    Logger.info("Starting fwup. port is #{inspect port}")
     {:ok, %{port: port, cm: cm, sender: nil, from: nil, sent_exit: false}}
   end
 
-  def handle_call(_cmd, _from, %{port: nil} = state) do
+  def handle_call(_cmd, _from, %{sent_exit: true} = state) do
     {:reply, :error, state}
   end
+  def handle_call({:send, _chunk}, _from, %{from: inprogress_from} = state) when inprogress_from != nil do
+    exit(:cant_handle_multiple_sends)
+  end
   def handle_call({:send, chunk}, from, state) do
-    Logger.info("#{inspect self()} Sending chunk of size #{byte_size(chunk)} to #{inspect state.port}...")
-    try do
-      {:ok, pid} = Task.start_link(fn() ->
-        Logger.info("#{inspect self()} calling Port.command")
-        Port.command(state.port, chunk)
-        Logger.info("#{inspect self()} done Port.command")
-      end)
-      Logger.info("#{inspect self()} Starting task...")
-      {:noreply, %{state | sender: pid, from: from}}
-    rescue
-      ArgumentError ->
-          Logger.error("Got an argument error!!")
-          {:reply, :error, %{state | port: nil}}
-    catch
-      :exit, _ ->
-          Logger.error("Sending data to fwup failed due to epipe")
-          {:reply, :error, %{state | port: nil}}
-    end
+    # Since fwup may be slower than ssh, we need to provide backpressure
+    # here. It's tricky since `Port.command/2` is the only way to send
+    # bytes to fwup synchronously, but it's possible for fwup to error
+    # out when it's sending. If fwup errors out, then we need to make
+    # sure that a message gets back to the user for what happened.
+    # `Port.command/2` exits on error (it will be an :epipe error).
+    # Therefore we start a new process to call `Port.command/2` while
+    # we continue to handle responses. We also trap_exit to get messages
+    # when the port the Task exit.
+    {:ok, pid} = Task.start_link(fn() ->
+      Port.command(state.port, chunk)
+    end)
+    {:noreply, %{state | sender: pid, from: from}}
   end
 
   def handle_info({port, {:data, response}}, %{port: port} = state) do
-    Logger.info("fwup got #{inspect response}")
     :ok = :ssh_channel.cast(state.cm, {:fwup_data, response})
     {:noreply, state}
   end
   def handle_info({port, {:exit_status, status}}, %{port: port} = state) do
     Logger.info("fwup exited with status #{status}")
-    state.sent_exit || :ssh_channel.cast(state.cm, {:fwup_exit, status})
-    {:noreply, %{state | sent_exit: true}}
+    new_state = maybe_send_fwup_exit(state, status)
+    {:noreply, new_state}
   end
   def handle_info({:EXIT, port, reason}, %{port: port} = state) do
-    Logger.error("Caught an EXIT from #{inspect port} for reason #{inspect reason}")
-    state.sent_exit || :ssh_channel.cast(state.cm, {:fwup_exit, 255})
+    Logger.error("nerves_firmware_ssh: unexpected exit from fwup: #{inspect reason}")
+    Process.send_after(self(), :send_exit, 1000)
     {:noreply, %{state | sent_exit: true}}
   end
   def handle_info({:EXIT, pid, :normal}, %{sender: pid} = state) do
-    Logger.info("#{inspect self()} Sending chunk done")
+    # Normal case where `Port.command/2` succeeds
     GenServer.reply(state.from, :ok)
     {:noreply,  %{state | sender: nil, from: nil}}
   end
   def handle_info({:EXIT, pid, reason}, %{sender: pid} = state) do
-    Logger.error("Caught an EXIT from sender #{inspect pid} for reason #{inspect reason}")
-    state.sent_exit || :ssh_channel.cast(state.cm, {:fwup_exit, 255})
-    GenServer.reply(state.from, :error)
-    {:noreply, %{state | sent_exit: true, sender: nil, from: nil}}
+    # Unexpected case where `Port.command/2` fails
+    Logger.error("nerves_firmware_ssh: unexpected send failure to fwup: #{inspect reason}")
+
+    # The :epipe from fwup exiting beats the final bytes sent by
+    # fwup. This delay lets those be received.
+    Process.send_after(self(), :send_exit, 1000)
+
+    {:noreply, %{state | sender: nil, sent_exit: true}}
   end
+  def handle_info(:send_exit, state) do
+    if state.from do
+      GenServer.reply(state.from, :error)
+    end
+    :ssh_channel.cast(state.cm, {:fwup_exit, 255})
+    {:noreply, %{state | from: nil}}
+  end
+
+  def maybe_send_fwup_exit(%{sent_exit: false} = state, status) do
+    :ssh_channel.cast(state.cm, {:fwup_exit, status})
+    %{state | sent_exit: true}
+  end
+  def maybe_send_fwup_exit(state, _status), do: state
+
+
 end
