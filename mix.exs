2fa3ac4630d6afa1b043891960eee13b44d8c802
Frank Hunleth <fhunleth@troodon-software.com>
2018-08-08 09:30:18 -0400

    v0.3.3 release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Nerves.Firmware.SSH.Mixfile do
   use Mix.Project
 
-  @version "0.3.2"
+  @version "0.3.3"
 
   @description """
   Perform over-the-air updates to Nerves devices using ssh
